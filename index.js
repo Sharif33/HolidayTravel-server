f86599beb61e1ba46d8b59722dd26b5f4b938cdc
Sharif33 <sharif.cse.nu@gmail.com>
2021-10-30 23:40:49 +0600

    delete get post added

--- index.js
+++ index.js
@@ -20,12 +20,20 @@ async function run() {
         await client.connect();
         const database = client.db('resortManagement');
         const resortCollection = database.collection('resorts');
+        const MyOrder = database.collection('orders');
+
 
         // GET Resorts
         app.get('/resorts', async (req, res) => {
             const cursor = resortCollection.find({});
             const resorts = await cursor.toArray();
             res.send(resorts);
+        });
+        // GET orders
+        app.get('/orders', async (req, res) => {
+            const cursor = MyOrder.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
         });
 
         // GET Single Resort
@@ -36,6 +44,31 @@ async function run() {
             const resort = await resortCollection.findOne(query);
             res.json(resort);
         })
+        /*  //GET orders by email
+         app.get('/orders/:email', async (req, res) => {
+             const result = await MyOrder.find({
+                 email = req.params.email
+             }).toArray();
+             res.send(result);
+         }); */
+
+        // get all order by email
+        app.get("/myOrders/:email", (req, res) => {
+            console.log(req.params);
+            MyOrder
+                .find({ email: req.params.email })
+                .toArray((err, results) => {
+                    res.send(results);
+                });
+        });
+
+        //DELETE my order
+        app.delete('/myOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await MyOrder.deleteOne(query);
+            res.json(result);
+        })
 
         // POST Resort
         app.post('/resorts', async (req, res) => {
@@ -45,12 +78,20 @@ async function run() {
             console.log(result);
             res.json(result)
         });
+        // POST orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log('hit the post api', order);
+            const result = await MyOrder.insertOne(order);
+            console.log(result);
+            res.json(result)
+        });
 
-        // DELETE API
-        app.delete('/resorts/:id', async (req, res) => {
+        // DELETE orders from ManageOrders
+        app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const result = await resortCollection.deleteOne(query);
+            const result = await MyOrder.deleteOne(query);
             res.json(result);
         })
 
